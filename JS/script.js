1b43a2eb983eb56edb69ad2b0bff07b53691e66e
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-29 08:02:37 +0200

    added js steps & DOM elements

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,17 @@
 console.log('JS OK')
+
+//* STEPS
+
+/* 
+
+* 1. Recuperare gli elementi di interesse all'interno del DOM.
+
+* 2. Creare un array.
+    ? 2a Inserire al suo interno una lista di oggeti per ciascuno dei i dati che sono stati forniti. 
+
+* 3. Stampare in console le informazioni nel ordine seguente: nome e cognome, ruolo e la stringa della foto.
+
+* 4. Stampare sul DOM le informazioni nel ordine seguente: nome e cognome, ruolo e la stringa della foto.
+
+
+*/
